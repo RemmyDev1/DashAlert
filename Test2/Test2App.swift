3ceab126f2df880d4c9f6a7968de46668bdcea84
Remmy <Kariemhassan20092gmail.com>
2025-01-27 15:23:57 -0800

    Initial Commit

---
+++ Test2/Test2App.swift
@@ -0,0 +1,17 @@
+//
+//  Test2App.swift
+//  Test2
+//
+//  Created by kariem on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
