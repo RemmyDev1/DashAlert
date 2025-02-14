b87f7254f6b2379d60d26f7c184e994467ded0e3
RemmyDev <85520351+RemmyDev1@users.noreply.github.com>
2025-02-13 16:20:30 -0800

    Tiny Fix .swiftpm 1
    fixed ThemeManager

--- DashAlert.swiftpm/ThemeManager.swift
+++ DashAlert.swiftpm/ThemeManager.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 //Manages Light and Dark Theme's in the app so when ever you change the theme it auto refreshes without having to click on another tab to see the changed theme
 class ThemeManager: ObservableObject {
-    @AppStorage("isDarkMode") var isDarkMode = false {
+    @AppStorage("isDarkMode") var isDarkMode = true{
         willSet { objectWillChange.send() } // Forces refresh when changed
     }
 }
